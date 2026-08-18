3831c35f4e6c773abd03eb3651b1e5d18c8dc155
Mond <omgdigital.developer.02@gmail.com>
2026-08-18 17:31:21 +0700

    อ

--- hash-and-array/easy/majority_element.py
+++ hash-and-array/easy/majority_element.py
@@ -8,9 +8,19 @@ You may assume that the majority element always exists in the array.
 """
 
 def majority_element(nums: list[int]) -> int:
-    # TODO: เขียนโค้ดตรงนี้
-    pass
-
+    count_map = {}
+    majority_threshold = len(nums) // 2  # เกินครึ่งหนึ่ง (n / 2)
+    
+    for num in nums:
+        if num not in count_map:
+            count_map[num] = 1
+        else:
+            count_map[num] += 1
+            
+        # ถ้าตัวไหนนับแล้วเกินครึ่ง ส่งคำตอบได้ทันที
+        if count_map[num] > majority_threshold:
+            return num
+        
 # Test cases
 print(majority_element([3, 2, 3]))             # Expected: 3
 print(majority_element([2, 2, 1, 1, 1, 2, 2])) # Expected: 2
